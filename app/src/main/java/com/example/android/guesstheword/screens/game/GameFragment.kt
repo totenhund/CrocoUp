b72a0dff5ad0afa69d176001bce92da15c4fad03
totenhund <caligoyan@gmail.com>
2020-12-30 23:59:26 +0600

    bug with rotate crash is fixed

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -25,6 +25,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.GameFragmentBinding
@@ -52,7 +53,7 @@ class GameFragment : Fragment() {
         )
 
         Timber.i("ViewModelProvider is Called!")
-        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
 
 
