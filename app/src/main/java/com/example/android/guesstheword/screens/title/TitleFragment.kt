832778afc43ad7deefc49c87cd00aacfb3df070d
totenhund <caligoyan@gmail.com>
2021-01-26 20:55:56 +0600

    Multiple language support

--- app/src/main/java/com/example/android/guesstheword/screens/title/TitleFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/title/TitleFragment.kt
@@ -22,6 +22,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -32,9 +33,12 @@ import androidx.viewpager2.widget.ViewPager2
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.database.*
 import com.example.android.guesstheword.databinding.TitleFragmentBinding
+import com.google.mlkit.common.model.DownloadConditions
+import com.google.mlkit.nl.translate.TranslateLanguage
+import com.google.mlkit.nl.translate.Translation
+import com.google.mlkit.nl.translate.TranslatorOptions
 import timber.log.Timber
 import kotlin.math.abs
-
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -44,13 +48,6 @@ import kotlinx.coroutines.launch
  */
 class TitleFragment : Fragment() {
 
-    // TODO database should be prepopulate
-    var animals = arrayOf("Cat","Cow","Sheep","Rabbit","Duck","Chicken","Horse","Squirrel","Snail","Mouse","chameleon","Deer","raccoon","Snake","Bat","Turtle","Octopus","Frog","whale","crab","clam","fish","lobster","shark","seahorse","desert","Lion","elephant","Cheetah")
-    var jobs = arrayOf("Astronaut","Chef","Construction","Firefighter","Doctor","Policia","Teacher","Nurse", "veterinarian","actress","actor","architect","singer","dentist","detective","writer", "farmer","nurse","pilot","engineer","accountant","butcher","cashier","barber", "carpenter","lifeguard","baker","electrician", "flight","attendant","plumber","receptionist", "researcher","scientist","lawyer","bus","driver","designer", "journalist","photographer","musician","painter", "florist","sales","assistant","mechanic","model","shop","assistant","politician", "translator","postman","hairdresser","taxi","driver","pharmacist","nanny", "travel","agent","cleaner","biologist","businesswoman", "businessman","dancer","gardener","meteorologist","programmer","travel","guide","saleswoman","salesman")
-    var idioms = arrayOf("Johnny Depp","Arnold Schwarzenegger","Jim Carrey","Emma Watson","Daniel Radcliffe","Leonardo DiCaprio","Tom Cruise","Brad Pitt","Charles Chaplin","Morgan Freeman","Tom Hanks","Hugh Jackman","Matt Damon","Sylvester Stallone","Will Smith","Clint Eastwood","Cameron Diaz","George Clooney","Steven Spielberg","Harrison Ford","Robert De Niro","Al Pacino","Robert Downey Jr.","Russell Crowe","Liam Neeson","Kate Winslet","Mark Wahlberg","Natalie Portman","Pierce Brosnan","Sean Connery","Orlando Bloom","Dwayne Johnson","Jackie Chan","Angelina Jolie","Adam Sandler","Scarlett Johansson","Heath Ledger","Anne Hathaway","Jessica Alba","Edward Norton","Keira Knightley","Bradley Cooper","Will Ferrell","Julia Roberts","Nicolas Cage","Daniel Craig","Keanu Reeves","Ian McKellen","Halle Berry","Bruce Willis","Samuel L. Jackson","Ben Stiller","Tommy Lee Jones","Antonio Banderas","Denzel Washington","Steve Carell","Shia LaBeouf","Megan Fox","James Franco","Mel Gibson","Vin Diesel","Tim Allen","Robin Williams",
-            "Kevin Spacey","Jason Biggs","Seann William Scott","Jean-Claude Van Damme","Zach Galifianakis","Owen Wilson","Christian Bale","Peter Jackson","Sandra Bullock","Bruce Lee","Drew Barrymore","Macaulay Culkin","Jack Nicholson","Bill Murray","Sigourney Weaver","Jake Gyllenhaal","Kamal Haasan","Jason Statham","Jet Li","Kate Beckinsale","Rowan Atkinson","Marlon Brando","John Travolta","Channing Tatum","Ben Affleck","Shah Rukh Khan","Jennifer Aniston","Emma Stone","Chris Hemsworth","James McAvoy","James Cameron","Amitabh Bachchan","Brendan Fraser","Rachel McAdams","Tom Hiddleston","Aamir Khan","Rajinikanth")
-    var icons = arrayOf(0x1F98A, 0x1F4BC, 0x1F929)
-    var colors = arrayOf("#faf0af", "#a3ddcb", "#cd5d7d")
 
     private lateinit var titleViewModel: TitleViewModel
     private lateinit var viewModelFactory: TitleViewModelFactory
@@ -60,10 +57,6 @@ class TitleFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: TitleFragmentBinding = DataBindingUtil.inflate(
                 inflater, R.layout.title_fragment, container, false)
-
-
-//
-
 
 
         viewModelFactory = TitleViewModelFactory(activity!!.application)
@@ -92,7 +85,6 @@ class TitleFragment : Fragment() {
         })
 
 
-
         val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
         val currentItemHorizontalMarginPx = resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
         val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
@@ -109,6 +101,29 @@ class TitleFragment : Fragment() {
 
         activity!!.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
+        val options = TranslatorOptions.Builder()
+                .setSourceLanguage(TranslateLanguage.ENGLISH)
+                .setTargetLanguage(TranslateLanguage.RUSSIAN)
+                .build()
+
+        val englishRussianTranslator = Translation.getClient(options)
+
+        var conditions = DownloadConditions.Builder()
+                .requireWifi()
+                .build()
+        englishRussianTranslator.downloadModelIfNeeded(conditions)
+                .addOnSuccessListener {
+                    englishRussianTranslator.translate("Actor Johnny Depp")
+                            .addOnSuccessListener { translatedText ->
+                                Toast.makeText(activity!!, translatedText, Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener { exception ->
+                                Timber.i("Not translated")
+                            }
+                }
+                .addOnFailureListener { exception ->
+                    Timber.i("model is not downloaded")
+                }
 
         return binding.root
     }
